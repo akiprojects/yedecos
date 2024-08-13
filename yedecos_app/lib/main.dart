72e2206a8c5b934e8aa87af95a17fd3608a9a312
leonheart0910 <wbs090801@gmail.com>
2024-08-13 23:00:59 +0900

    feat : add splash screen

--- yedecos_app/lib/main.dart
+++ yedecos_app/lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'dart:async'; // Future.delayed를 사용하기 위해 필요
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(Duration(seconds: 1)); // 1초 동안 스플래시 화면 표시
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('hello')
+      home: Scaffold(
+        body: Center(
+          child: Text('hello'),
+        ),
+      ),
     );
   }
 }
